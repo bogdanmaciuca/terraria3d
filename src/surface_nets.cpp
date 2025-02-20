c42a95c59a7cacec7e96888873a1340d4b74d28c
bogdanmaciuca <bogdanmaciuca0621@gmail.com>
2025-02-20 20:00:31 +0200

    semi-optimized normals for surface nets

--- src/surface_nets.cpp
+++ src/surface_nets.cpp
@@ -24,29 +24,53 @@ void SurfaceNets::CreateMesh(const Chunk& chunk) {
                     corner_values[i] = weight;
                     corner_signs |= (1 << i) * ((weight & (1 << 7)) == 0);
                 }
-                if (x != 0 && y != 0 && z != 0 && x != ChunkSize-1 && y != ChunkSize-1 && z != ChunkSize-1) {
-                    for (i32 i = 0; i < 8; i++) {
-                        corner_normals[i] = glm::vec3(
-                                Val(x + cornersInt3[i][0] - 1, y + cornersInt3[i][1], z + cornersInt3[i][2]) -  Val(x + cornersInt3[i][0] + 1, y + cornersInt3[i][1], z + cornersInt3[i][2]),
-                                Val(x + cornersInt3[i][0], y + cornersInt3[i][1] - 1, z + cornersInt3[i][2]) -  Val(x + cornersInt3[i][0], y + cornersInt3[i][1] + 1, z + cornersInt3[i][2]),
-                                Val(x + cornersInt3[i][0], y + cornersInt3[i][1], z + cornersInt3[i][2] - 1) -  Val(x + cornersInt3[i][0], y + cornersInt3[i][1], z + cornersInt3[i][2] + 1)
-                                );
-                    }
-                }
                 const float edge_num = coefficients_sizes[corner_signs];
                 if (edge_num > 0) {
-                    Vertex vert = { .pos = glm::vec3(0) };
-                    const auto coef = coefficients[corner_signs];
+                    if (x != 0 && y != 0 && z != 0 && x != ChunkSize-1 && y != ChunkSize-1 && z != ChunkSize-1) {
+                        for (i32 i = 0; i < 8; i++) {
+                            corner_normals[i] = glm::vec3(
+                                    Val(x + cornersInt3[i][0] - 1, y + cornersInt3[i][1], z + cornersInt3[i][2]) -  Val(x + cornersInt3[i][0] + 1, y + cornersInt3[i][1], z + cornersInt3[i][2]),
+                                    Val(x + cornersInt3[i][0], y + cornersInt3[i][1] - 1, z + cornersInt3[i][2]) -  Val(x + cornersInt3[i][0], y + cornersInt3[i][1] + 1, z + cornersInt3[i][2]),
+                                    Val(x + cornersInt3[i][0], y + cornersInt3[i][1], z + cornersInt3[i][2] - 1) -  Val(x + cornersInt3[i][0], y + cornersInt3[i][1], z + cornersInt3[i][2] + 1)
+                                    );
+                        }
+                    }
+
+                    Vertex vert = { .pos = glm::vec3(0), .normal = glm::vec3(0) };
+                    const auto edges = edges_list[corner_signs];
                     for (i32 i = 0; i < edge_num; i++) {
-                        i8 v0 = corner_values[coef[i][0]];
-                        i8 v1 = corner_values[coef[i][1]];
+                        i8 v0 = corner_values[edges[i][0]];
+                        i8 v1 = corner_values[edges[i][1]];
                         float lerp_t = static_cast<float>((surface - v0)) / (v1 - v0);
-                        vert.pos += corners[coef[i][0]] + lerp_t * (corners[coef[i][1]] - corners[coef[i][0]]);
-                        vert.normal += corner_normals[coef[i][0]] + lerp_t * (corner_normals[coef[i][1]] - corner_normals[coef[i][0]]);
+                        vert.pos += corners[edges[i][0]] + lerp_t * (corners[edges[i][1]] - corners[edges[i][0]]);
+                        vert.normal += corner_normals[edges[i][0]] + lerp_t * (corner_normals[edges[i][1]] - corner_normals[edges[i][0]]);
                     }
                     vert.pos /= edge_num;
                     vert.pos += glm::vec3(x, y, z);
-                    vert.normal /= edge_num;
+
+                    // Use this for shaving 0.5ms off (in turn for not so smooth normals)
+                    // TODO: Maybe make a macro for this
+                    //constexpr u8 edgesOrdered[12][2] = {
+                    //    { 0, 3 }, { 1, 2 }, { 4, 7 }, { 5, 6 }, // Ox
+                    //    { 0, 4 }, { 3, 7 }, { 2, 6 }, { 1, 5 }, // Oy
+                    //    { 0, 1 }, { 3, 2 }, { 4, 5 }, { 7, 6 }  // Oz
+                    //};
+                    //for (int i = 0; i < 4; i++) {
+                    //    vert.normal.x += 
+                    //        Val(x + corners[edgesOrdered[i][1]].x, y + corners[edgesOrdered[i][1]].y, z + corners[edgesOrdered[i][1]].z) -
+                    //        Val(x + corners[edgesOrdered[i][0]].x, y + corners[edgesOrdered[i][0]].y, z + corners[edgesOrdered[i][0]].z);
+                    //}
+                    //for (int i = 4; i < 8; i++) {
+                    //    vert.normal.y += 
+                    //        Val(x + corners[edgesOrdered[i][1]].x, y + corners[edgesOrdered[i][1]].y, z + corners[edgesOrdered[i][1]].z) -
+                    //        Val(x + corners[edgesOrdered[i][0]].x, y + corners[edgesOrdered[i][0]].y, z + corners[edgesOrdered[i][0]].z);
+                    //}
+                    //for (int i = 8; i < 12; i++) {
+                    //    vert.normal.z += 
+                    //        Val(x + corners[edgesOrdered[i][1]].x, y + corners[edgesOrdered[i][1]].y, z + corners[edgesOrdered[i][1]].z) -
+                    //        Val(x + corners[edgesOrdered[i][0]].x, y + corners[edgesOrdered[i][0]].y, z + corners[edgesOrdered[i][0]].z);
+                    //}
+
                     _cell_indices[Pos(x, y, z)] = _vertices.size();
                     _vertices.push_back(vert);
                     if (edge_num > 1) {
